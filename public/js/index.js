e0c315cac3d7f5a047e1b909bda14380ceabcd6e
Francisco Khaue Dantas Braga <khauebraga55@gmail.com>
2025-11-20 18:48:37 -0300

    Fazendo boas práticas com comentários

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,10 @@ const myModal = new bootstrap.Modal("#register-modal");
 let logged = sessionStorage.getItem("logged");
 const session = localStorage.getItem("session");
 
+
+// 1 - Verificar existencia de usuário já logado
+// 2 - Caso exista um usuário logado, mandar para a página home
+// 3 - Caso não, manter na página index
 checkLogged();
 
 //LOGAR NO SISTEMA
@@ -34,11 +38,14 @@ document.getElementById("login-form").addEventListener("submit", function(e){
 
 //CRIAR CONTA
 document.getElementById("create-form").addEventListener("submit", function(e){
+    // 1 - remove o recarregamento da página ao submeter um form
     e.preventDefault();
 
+    // 2 - Capturar os dados informados pelo usuário no form
     const email = document.getElementById("email-create-input").value;
     const password = document.getElementById("password-create-input").value;
 
+    // 3 - Verificar se o que foi digitado atende a os critérios mínimos
     if(email.length < 5){
         alert("Preencha o campo com e-mail válido.");
         return;
@@ -49,14 +56,17 @@ document.getElementById("create-form").addEventListener("submit", function(e){
         return;
     }
 
+    // 4 - Armazena os dados da conta no localStorage
     saveAccount({
         login: email,
         password: password,
         transactions: []
     });
 
+    // 5 -  Fechar o modal
     myModal.hide();
 
+    // 7 - Feedback para usuário
     alert("Conta criada com sucesso.");
 });
 
